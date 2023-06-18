cb6cfb920c2e416a95193cbfdacb4fa509a9ae08
Awinti Amin <awintiamine@gmail.com>
2023-06-18 23:59:42 +0100

    part1 - Exercises 1.3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,27 +1,27 @@
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
+      <Content part1={part1} part2={part2} part3={part3} />
       <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        exercises1={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
       />
     </div>
   );
@@ -35,18 +35,11 @@ const Part = ({ part, exercise }) => (
   </p>
 );
 
-const Content = ({
-  part1,
-  exercises1,
-  part2,
-  exercises2,
-  part3,
-  exercises3,
-}) => (
+const Content = ({ part1, part2, part3 }) => (
   <div>
-    <Part part={part1} exercise={exercises1} />
-    <Part part={part2} exercise={exercises2} />
-    <Part part={part3} exercise={exercises3} />
+    <Part part={part1.name} exercise={part1.exercises} />
+    <Part part={part2.name} exercise={part2.exercises} />
+    <Part part={part3.name} exercise={part3.exercises} />
   </div>
 );
 
