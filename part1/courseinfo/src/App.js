8aaf2b6a395233b06a7bd2841ef12b686e1bda87
Awinti Amin <awintiamine@gmail.com>
2023-06-19 00:08:07 +0100

    part1 - Exercise 1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,27 +1,29 @@
 const App = () => {
   const course = 'Half Stack application development';
 
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
+      <Content part1={parts[0]} part2={parts[1]} part3={parts[2]} />
       <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
+        exercises1={parts[0].exercises}
+        exercises2={parts[1].exercises}
+        exercises3={parts[2].exercises}
       />
     </div>
   );
