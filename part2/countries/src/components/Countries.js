d3258271538ef8c5ebdb8d932c9623f031d894d7
Awinti Amin <awintiamine@gmail.com>
2023-07-01 11:24:01 +0100

    Part2 - Exercise 2.19

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CountryDetails from './CountryDetails';
 
 const Countries = ({ countries }) => {
+  const [countryName, setCountryName] = useState(null);
+
   return (
     <>
-      {countries.map((c) => (
-        <p key={c.name.common}>{c.name.common}</p>
-      ))}
+      {countries.map((c, i) => {
+        if (c.name.common === countryName)
+          return <CountryDetails countries={countries[i]} />;
+        else
+          return (
+            <p key={c.name.common}>
+              {c.name.common}{' '}
+              <button onClick={() => setCountryName(c.name.common)}>
+                show
+              </button>
+            </p>
+          );
+      })}
     </>
   );
 };
