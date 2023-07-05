c6259fbf329c389354a81001daf70361ee1415fa
Awinti Amin <awintiamine@gmail.com>
2023-07-05 15:15:40 +0100

    Part3 - Exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,7 +5,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use(morgan('tiny'));
+morgan.token('data', (req) =>
+  req.method === 'POST' ? JSON.stringify(req.body) : ' '
+);
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :data')
+);
 
 let persons = [
   {
